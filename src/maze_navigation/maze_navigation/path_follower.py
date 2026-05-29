90350be4d24f28b6d935d49116487d46a6a9287f
reza0code <utbildningkonto2019@gmail.com>
2026-05-29 16:29:06 +0200

    Add basic odom based path following control

--- src/maze_navigation/maze_navigation/path_follower.py
+++ src/maze_navigation/maze_navigation/path_follower.py
@@ -2,17 +2,24 @@ import rclpy
 import math
 from rclpy.node import Node
 from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import Twist
 
+def yaw_from_quaternion(q):
+    siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
 
+    return math.atan2(siny_cosp, cosy_cosp)
 class PathFollower(Node):
     def __init__(self):
         super().__init__("path_follower")
         self.get_logger().info("Path follower node started")
+        self.cmd_pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
         self.path = []
         self.target_index = 0
-        self.robot_x = 0.25
-        self.robot_y = 0.25
+        self.robot_x = 0.0
+        self.robot_y = 0.0
+        self.robot_yaw = 0.0
         self.goal_logged = False
 
         self.path_sub = self.create_subscription(
@@ -46,7 +53,11 @@ class PathFollower(Node):
     def odom_callback(self, msg):
         self.robot_x = msg.pose.pose.position.x
         self.robot_y = msg.pose.pose.position.y
+        self.robot_yaw = yaw_from_quaternion(msg.pose.pose.orientation)
 
+        #self.get_logger().info(
+         #   f"Odom: x={self.robot_x:.3f}, y={self.robot_y:.3f}, yaw={self.robot_yaw:.3f}"
+        #)
     def control_loop(self):
         if not self.path:
             return
@@ -58,27 +69,43 @@ class PathFollower(Node):
             return
 
         target_pose = self.path[self.target_index].pose.position
-        
+
         distance = math.sqrt(
             (target_pose.x - self.robot_x) ** 2 +
             (target_pose.y - self.robot_y) ** 2
         )
+
         if distance < 0.1 and self.target_index < len(self.path) - 1:
             self.target_index += 1
             self.get_logger().info(
                 f"Moving to next target index: {self.target_index}"
             )
             return
-        
 
-        self.get_logger().info(
-            f"Distance to target: {distance:.3f}"
+        target_angle = math.atan2(
+            target_pose.y - self.robot_y,
+            target_pose.x - self.robot_x
         )
 
+        angle_error = target_angle - self.robot_yaw
+        angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+
         self.get_logger().info(
-            f"Current target index: {self.target_index}, "
-            f"x={target_pose.x}, y={target_pose.y}"
+            f"index={self.target_index}, "
+            f"distance={distance:.3f}, "
+            f"angle_error={angle_error:.3f}"
         )
+
+        twist = Twist()
+
+        if abs(angle_error) > 0.05:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.8 * angle_error  
+        else:
+            twist.linear.x = 0.05
+            twist.angular.z = 0.0
+
+        self.cmd_pub.publish(twist)
         #self.robot_x = target_pose.x
         #self.robot_y = target_pose.y
 
